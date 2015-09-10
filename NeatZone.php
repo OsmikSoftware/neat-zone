2d5ccbe159b21e53c3712f0c62c78bbcccc95fcc
jenki221 <jenki221@gmail.com>
2015-09-10 08:26:06 -0400

    added compatibility for int timezones & dst

--- NeatZone.php
+++ NeatZone.php
@@ -6,12 +6,25 @@ class NeatZone{
         if(is_string($zone)){
             $this->DisplayedZone = $this->getNeatZoneForString($zone);
         }
+        //if int offset
+        if(is_int($zone)){
+            $offset = $zone;
+            //check daylight savings time
+            if(date('I')){
+                $offset -= 1;
+            }
+            $this->DisplayedZone = $this->getNeatZoneForString(timezone_name_from_abbr("", $offset*3600, false));
+        }
     }
     private function getNeatZoneForString($zone){
         $zones = array(
             'America/Los_Angeles'=>"Los Angeles, CA",
             'America/Detroit'=>"Detroit, MI"
         );
+        //if not found in our list, return the default
+        if(!in_array($zone, $zones)){
+            return $zone;
+        }
         return $zones[$zone];
     }
     public function print_zone(){
